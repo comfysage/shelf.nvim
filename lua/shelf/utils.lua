012fc13de2b932f0eae7f30cfc4e4abd8a9ac8b6
CrispyBaccoon <67917529+CrispyBaccoon@users.noreply.github.com>
2024-01-12 16:31:04 +0100

    add(utils): add `utils.getheight()`

--- lua/shelf/utils.lua
+++ lua/shelf/utils.lua
@@ -4,4 +4,34 @@ utils.reorder = function(bufnr, nbufnr) end
 
 utils.simplify = function() end
 
+local function _getids(layout)
+  local _layout = layout
+  if layout[1] == 'leaf' then
+    return { layout[2] }
+  elseif layout[1] == 'row' then
+    return { layout[2][1][2] }
+  end
+  local ids = {}
+  for _, l in ipairs(layout[2]) do
+    local _ids = _getids(l)
+    local j = #ids
+    for i, id in ipairs(_ids) do
+      ids[i+j] = id
+    end
+  end
+  return ids
+end
+
+utils.getheight = function()
+  local layout = vim.fn.winlayout()
+  local id = _getids(layout)
+  local sum = 0
+  vim.tbl_map(function(_id)
+    local height = vim.api.nvim_win_get_height(_id)
+    sum = sum + height
+  end, id)
+
+  return sum
+end
+
 return utils
