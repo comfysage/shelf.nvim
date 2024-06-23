21e62e36da7336cd960b29ddf985ecc963cf2845
comfysage <67917529+comfysage@users.noreply.github.com>
2024-06-23 22:43:09 +0200

    fix(utils): set buflisted on new buffers

--- lua/shelf/utils.lua
+++ lua/shelf/utils.lua
@@ -5,6 +5,7 @@ local utils = {}
 utils.create_buf = function(name)
   vim.notify(('creating buffer [%s]'):format(name), vim.log.levels.DEBUG)
   local nr = vim.fn.bufadd(name)
+  vim.api.nvim_set_option_value('buflisted', true, {buf=nr})
   vim.api.nvim_buf_call(nr, function()
     vim.cmd.buffer()
   end)
