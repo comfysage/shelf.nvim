7b5a65960ab4d0f408abeacf8ac123450ce73688
robin <comfysage@isabelroses.com>
2025-06-11 22:12:45 +0100

    fix(utils): remove unnecessary debug

--- lua/shelf/utils.lua
+++ lua/shelf/utils.lua
@@ -3,7 +3,6 @@ local utils = {}
 ---@param name string
 ---@return integer
 utils.create_buf = function(name)
-  vim.notify(('creating buffer [%s]'):format(name), vim.log.levels.DEBUG)
   local nr = vim.fn.bufadd(name)
   vim.api.nvim_set_option_value('buflisted', true, {buf=nr})
   vim.api.nvim_buf_call(nr, function()
